c39f10ffaac9e00350c3b6932207ada2a4b537f8
Sophia Hadash <sophiahadash@gmail.com>
2020-03-06 00:36:42 +0100

    Add RAM usage LED

--- src/main.py
+++ src/main.py
@@ -12,7 +12,9 @@ client = docker.from_env()
 
 # visualize active containers
 while(True):
+    print(client.containers.list())
     for container in client.containers.list():
+        print(container)
         if container.attrs['Config']['Image']=="angelnu/glusterfs:latest":
             set_pixel(0, 0, 0, 255)
         if container.attrs['Config']['Image']=="shadash/docker-multiarch-jenkins:latest":
@@ -24,10 +26,11 @@ while(True):
         if container.attrs['Config']['Image']=="nginx:latest":
             set_pixel(4, 255, 0, 255)
 
-    # green on LED 5 (debug)
-    set_pixel(5, 0, 255, 0)
+    # disk usage green (0%) to red (100%) on LED 5
+    disku = int((psutil.disk_usage('/dev/sda1').percent/100)*255)
+    set_pixel(5, disku, 255 - disku, 0)
 
-    # RAM usage green (0%) to red (100%) on LED 7
+    # RAM usage green (0%) to red (100%) on LED 6
     mem = psutil.virtual_memory()
     memu = int((mem.percent/100)*255)
     set_pixel(6, memu, 255 - memu, 0)
@@ -37,4 +40,4 @@ while(True):
     set_pixel(7, cpu, 255-cpu, 0)
 
     show()
-    time.sleep(0.5)
+    time.sleep(0.01)
