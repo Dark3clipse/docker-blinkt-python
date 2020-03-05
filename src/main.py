97f558a79f2e0b7b43801a41e99882b962587d6b
Sophia Hadash <sophiahadash@gmail.com>
2020-03-06 00:10:15 +0100

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 from blinkt import set_pixel, set_brightness, show, clear
 import time
+import docker
 
+# reset gpio
 set_brightness(0.1)
 clear()
+
+# initialize docker-client
+client = docker.from_env()
+print(client.containers.list())
+
+
 set_pixel(1, 255, 255, 255)
 show()
 
 while(True):
-    time.sleep(1);
+    time.sleep(1)
